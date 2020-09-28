9fe27a8bbc663fd09d10c3742c4fdb354e0eb2f7
Michael Joses Dharmali <michael.535180037@stu.untar.ac.id>
2020-09-28 15:33:56 +0700

    Tugas Pertemuan 4

---
+++ Pertemuan 4 - Javascript/script.js
@@ -0,0 +1,6 @@
+function changeLine1Style() {
+    document.getElementById("p1").className = "MakeRed";
+}
+function ChangeStyle(id, classname){
+    document.getElementById(id).className = classname;
+}
