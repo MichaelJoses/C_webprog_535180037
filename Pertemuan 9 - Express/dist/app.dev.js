12dbad02091b871ded8c48fdd4d9375db81705a4
Michael Joses Dharmali <michael.535180037@stu.untar.ac.id>
2020-11-04 19:36:20 +0700

    Menambahkan Pertemuan 9 - Express

---
+++ Pertemuan 9 - Express/dist/app.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var express = require('express');
+
+express().listen('3000', function () {
+  console.log("server ini berjalan");
+});
---
+++ Pertemuan 9 - Express/index.js
@@ -0,0 +1,25 @@
+var express = require ('express')
+
+var app = express()
+
+app.get('/', function(req, res){
+    res.send('Helloworld')
+})
+
+app.post('/', function(req, res){
+    res.send('ini adalah post req')
+})
+
+app.put('/', function(req, res){
+    res.send('put req telah dijalankan')
+})
+
+app.delete('/', function(req, res){
+    res.send('delete request pada suatu user!')
+})
+
+app.use(express.static('public'));
+
+//specified port
+
+app.listen(3000)
